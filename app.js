c465356533cecdce66a6e6e26063a270af2e3fc2
archeelp <archeelparekh11@gmail.com>
2020-12-19 17:02:12 +0530

    added dfff

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ var express					= require("express"),
 	databaseURL 			= process.env.DATABASEURL || 'mongodb://localhost/clinicapp';
 	arcgisRestGeocoding = require('@esri/arcgis-rest-geocoding'),
 	{ geocode } = arcgisRestGeocoding,
-	secret=process.env.SECRET||"We are clinicapp devlopers";
+	secret=process.env.SECRET||"We are clinicapp devlopers",
+	dfff = require('dialogflow-fulfillment');
 
 mongoose.connect(databaseURL, { useNewUrlParser: true });
 app.use(express.static('pubic'));
@@ -868,6 +869,72 @@ app.get("/patienthome/:id",isLoggedIn,ispatient, function(req, res){
     });
 });
 
+app.post('/chatBot', (req, res)=>{
+    const agent = new dfff.WebhookClient({
+        request : req,
+        response : res
+    });
+
+    function demo(agent){
+        agent.add("Sending response from Webhook server as v1.1.11.1");
+    }
+    function customPayloadDemo(agent){
+        var payloadData = {
+            "richContent": [
+              [
+                {
+                  "type": "accordion",
+                  "title": "Accordion title",
+                  "subtitle": "Accordion subtitle",
+                  "image": {
+                    "src": {
+                      "rawUrl": "https://example.com/images/logo.png"
+                    }
+                  },
+                  "text": "Accordion text"
+                }
+              ]
+            ]
+          }
+
+          agent.add( new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))
+    }
+
+    function finalConfirmation(agent){
+      var name = agent.context.get("awaiting_name").parameters['given-name'];
+      var email = agent.context.get("awaiting_email").parameters.email;
+
+      console.log(name);
+      console.log(email);
+
+
+      
+
+      agent.add(`Hello ${name}, your email: ${email}. We confirmed your meeting.`);
+
+      return db.collection('meeting').add({
+        name : name,
+        email : email,
+        time : Date.now()
+      }).then(ref =>
+
+        //fetching free slots from G-cal
+        console.log("Meeting details added to DB")
+        )
+
+    }
+
+
+    var intentMap = new Map();
+    intentMap.set('finalConfirmation', finalConfirmation)
+    intentMap.set('webhookDemo',demo )
+    intentMap.set('customPayloadDemo', customPayloadDemo)
+    
+
+    agent.handleRequest(intentMap);
+
+});
+
 app.get("/*", function(req, res){
 	req.flash("error","Error 404! The page you are looking for is not found.");
 	res.redirect("/");
@@ -927,4 +994,4 @@ function escapeRegex(text) {
 
 app.listen(process.env.PORT||3000, function(){
 	console.log("The Clinicapp Server Has Started!");
- })
+})
